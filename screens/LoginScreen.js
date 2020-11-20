793942e0ec906278f7993f99033523afcc2069b9
josterexcv <60027256+josterexcv@users.noreply.github.com>
2020-11-20 10:31:55 +0400

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, TextInput, Text, StyleSheet, Image } from 'react-native';
+
+export default class LoginScreen extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+        );
+    }
+}
---
+++ screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, TextInput, Text, StyleSheet, Image } from 'react-native';
+
+export default class SignupScreen extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text>Signup Screen</Text>
+        </View>
+        );
+    }
+}
